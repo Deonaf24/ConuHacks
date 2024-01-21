c10a3b409717daef49508bb7c64290608cac6f4e
Deonaf24 <106830416+Deonaf24@users.noreply.github.com>
2024-01-20 19:57:23 -0500

    front end

--- website/backend.py
+++ website/backend.py
@@ -8,7 +8,7 @@ app = flask.Flask(__name__, template_folder=".")
 def my_maps():
 	mapbox_access_token = 'pk.eyJ1IjoiamVyZW1pYXMyMTMiLCJhIjoiY2xybWY1OTN6MTBpMjJrcDc0c3Z1Z3RzbiJ9.JAmfJcUFTUBaoVPQ7fvTyQ'
 
-	return render_template('index.html',
+	return render_template('gamescreen.html',
 		mapbox_access_token=mapbox_access_token)
 
 if __name__ == '__main__':
